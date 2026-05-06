a3659c671d1d08e3c41eb2826765fdc11d3bb65c
Sitt Guruvanich <aekazitt@gmail.com>
2026-05-06 21:34:38 +0700

    feat: render markdown using leptos-md crate

--- src/pages/post.rs
+++ src/pages/post.rs
@@ -2,11 +2,13 @@
 
 // third-party crates
 use leptos::prelude::*;
+use leptos_md::Markdown;
 use leptos_meta::{Meta, Title};
 use leptos_router::hooks::use_params;
 use leptos_router::params::Params;
 
 // local modules
+use crate::components::ui::card::{Card, CardContent, CardDescription, CardHeader, CardTitle};
 use crate::files::posts::fetch_post;
 
 #[derive(Params, Clone, Debug, PartialEq, Eq)]
@@ -17,7 +19,7 @@ pub struct PostParams {
 #[component]
 pub fn Post() -> impl IntoView {
   let params = use_params::<PostParams>();
-  let post_res = LocalResource::new(move || {
+  let resource = LocalResource::new(move || {
     let slug = params.get().ok().and_then(|q| q.slug);
     async move {
       match slug.as_deref() {
@@ -26,23 +28,40 @@ pub fn Post() -> impl IntoView {
       }
     }
   });
-
   view! {
     <Suspense fallback=|| view! { <p>"Loading post…"</p> }>
-      {move || match post_res.get() {
+      {move || match resource.get() {
         None => view! { <p>"Loading post…"</p> }.into_any(),
-        Some(Ok(Some(post))) => view! {
-          <article>
-            <h1>
-              {post.title.clone()}
-            </h1>
-            <p>
-              {post.content.clone()}
-            </p>
-            <Title text=post.title/>
-            <Meta name="description" content=post.content.clone()/>
+        Some(Ok(Some(post))) => {
+          let title = StoredValue::new(post.title);
+          let content = StoredValue::new(post.content);
+          view! {
+          <article
+            class="
+              max-w-2xl
+              px-auto
+            ">
+            <Card
+              class="
+                w-full
+              ">
+              <CardHeader>
+                <CardTitle>
+                  {title.get_value()}
+                </CardTitle>
+                <CardDescription>
+                  Sitt Guruvanich
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <Markdown content=content.get_value()/>
+              </CardContent>
+            </Card>
+            <Meta name="description" content=content.get_value()/>
+            <Title text=title.get_value()/>
           </article>
-        }.into_any(),
+        }.into_any()
+        },
         Some(Ok(None)) => view! {
           <p>"Post not found."</p>
         }.into_any(),
