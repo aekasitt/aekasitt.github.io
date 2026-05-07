d5fbf0b6527de0618bb7b6e1e09d80b3f63f88fc
Sitt Guruvanich <aekazitt@gmail.com>
2026-05-07 13:21:13 +0700

    feat: optimize binary size; drop leptos-md

--- src/pages/post.rs
+++ src/pages/post.rs
@@ -2,10 +2,11 @@
 
 // third-party crates
 use leptos::prelude::*;
-use leptos_md::Markdown;
 use leptos_meta::{Meta, Title};
 use leptos_router::hooks::use_params;
 use leptos_router::params::Params;
+use pulldown_cmark::Parser;
+use pulldown_cmark::html::push_html;
 
 // local modules
 use crate::components::ui::card::{Card, CardContent, CardDescription, CardHeader, CardTitle};
@@ -33,39 +34,48 @@ pub fn Post() -> impl IntoView {
       {move || match resource.get() {
         None => view! { <p>"Loading post…"</p> }.into_any(),
         Some(Ok(Some(post))) => {
+          let parser = Parser::new(&post.content);
+          let mut output = String::new();
+          push_html(&mut output, parser);
+          let content = StoredValue::new(output);
           let title = StoredValue::new(post.title);
-          let content = StoredValue::new(post.content);
           view! {
-          <article
-            class="
-              isolate
-              lg:px-16
-              pt-8
-              px-8
-              relative
-              w-full
-            ">
-            <Card>
-              <CardHeader>
-                <CardTitle
-                  class="
-                    font-bold
-                    text-2xl
-                  ">
-                  {title.get_value()}
-                </CardTitle>
-                <CardDescription>
-                  Sitt Guruvanich
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <Markdown content=content.get_value()/>
-              </CardContent>
-            </Card>
-            <Meta name="description" content=content.get_value()/>
-            <Title text=title.get_value()/>
-          </article>
-        }.into_any()
+            <article
+              class="
+                isolate
+                lg:px-16
+                pt-8
+                px-8
+                relative
+                w-full
+              ">
+              <Card>
+                <CardHeader>
+                  <CardTitle
+                    class="
+                      font-bold
+                      text-2xl
+                    ">
+                    {title.get_value()}
+                  </CardTitle>
+                  <CardDescription>
+                    Sitt Guruvanich
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div
+                    class="
+                      dark:prose-invert
+                      prose
+                    "
+                    inner_html=content.get_value()
+                  />
+                </CardContent>
+              </Card>
+              <Meta name="description" content=content.get_value()/>
+              <Title text=title.get_value()/>
+            </article>
+          }.into_any()
         },
         Some(Ok(None)) => view! {
           <p>"Post not found."</p>
